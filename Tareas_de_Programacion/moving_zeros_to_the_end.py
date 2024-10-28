d71d7ebc85ad1458ea6001dd6bff0b3a50b25169
Kaneki <alex123viri@gmail.com>
2024-10-28 22:54:16 +0100

    Actualizado 28/10/24

--- Tareas_de_Programacion/moving_zeros_to_the_end.py
+++ Tareas_de_Programacion/moving_zeros_to_the_end.py
@@ -1,8 +1,6 @@
 def move_zeros(lst):
     for num in lst:
         if num == 0:
-            lst.remove(0)
+            lst.remove()
             lst.append(0)
     return lst
-
-print(move_zeros([1, 0, 1, 2, 0, 1, 3]))
