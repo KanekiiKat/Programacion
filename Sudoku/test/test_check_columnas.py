fc9082cbbf691c1574166c8c6a8ee2d51ca3989e
Kaneki <alex123viri@gmail.com>
2024-10-31 19:05:46 +0100

    Actualizado

--- Sudoku/test/test_check_columnas.py
+++ Sudoku/test/test_check_columnas.py
@@ -1,3 +1,16 @@
 import pytest
 from src.checkcolumnas import checkcolumnas
 import casosTest.casosTestSudoku as casosTest
+
+@pytest.mark.columnas_validas
+@pytest.mark.parametrize("sudoku, sano",
+                         [  (casosTest.correcto, True),
+                            (casosTest.numero_repetido_fila_columna, False),
+                            (casosTest.numero_repetido_columna, False),
+                            (casosTest.numero_no_presente, False),
+                            pytest.param(casosTest.numero_fuera_del_rango,
+                                         False,
+                                         marks=pytest.mark.barricada)])
+
+def test_check_columnas(sudoku, sano):
+    assert checkcolumnas(sudoku) == sano
